e3f3748ccc0f221752e521f9c9d4f8a5e91ea1c3
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-10-07 19:15:53 -0300

    Started the implementation of the weather application, installed the libraries needed to assemble the project and created the WeatherBox component (without CSS stylization).

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="center">
+    <weather-box />
+  </div>
+</template>
+
+<script>
+import WeatherBox from '@/components/WeatherBox.vue'
+
+export default {
+  name: "App",
+  components: {
+    WeatherBox
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Ubuntu", sans-serif;
+}
+
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #0093e9;
+  background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+}
+</style>
---
+++ src/components/WeatherBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="form">
+      <h3>Confira o clima na sua cidade:</h3>
+      <div class="form-input-container">
+        <input type="text" placeholder="Digite o nome da cidade" />
+        <button>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+    </div>
+
+    <div class="weather-data">
+      <h2>
+        <i class="fa-solid fa-location-dot"></i>
+        <span>Poços de Caldas</span>
+        <img src="" alt="Bandeira do país" />
+      </h2>
+      
+      <p><span>38</span>&deg;C</p>
+
+      <div class="description-container">
+        <p>Nublado</p>
+        <img src="" alt="Condição do tempo" />
+      </div>
+
+      <div class="container-details">
+        <p>
+          <i class="fa-solid fa-droplet"></i>
+          <span>48%</span>
+        </p>
+        <p>
+          <i class="fa-solid fa-wind"></i>
+          <span>2.8Km/h</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherBox",
+};
+</script>
